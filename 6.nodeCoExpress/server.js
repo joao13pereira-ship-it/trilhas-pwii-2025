7f078873f69a0f9e4210fcf303b810a591431064
Joao Pereira <joao13.pereira@alunos.ifsuldeminas.edu.br>
2025-10-23 15:25:28 -0300

    2-2025-10-23

--- 6.nodeCoExpress/server.js
+++ 6.nodeCoExpress/server.js
@@ -4,3 +4,11 @@ const express = require('express');
 const app = express();
 // Define porta para o servidor
 const PORT=3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+app.get(`/`,(req,res) => {
+    res.send(' <h2>Pagina Principal!</h2>');
+});
