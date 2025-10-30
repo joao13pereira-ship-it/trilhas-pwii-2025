2d90b98b9fc2c0dfea02ea6da11f3db659ecca9c
Joao Pereira <joao13.pereira@alunos.ifsuldeminas.edu.br>
2025-10-30 14:37:06 -0300

    2025-10-30

--- 7.nodeExpressMySQL/server.js
+++ 7.nodeExpressMySQL/server.js
@@ -1,32 +1,30 @@
 // Importa o módulo Express
 const express = require("express");
-// importa o módulo do mysql
-const mysql = require('mysql2');
-app.use(express.json());
-
-
+// Importa o módulo MySQL
+const mysql = require("mysql2");
 // Cria uma aplicação Express
 const app = express();
-
+// Middleware para interpretar JSON
+app.use(express.json());
 // Define a porta do servidor
 const PORT = 3000;
 
-// Configuração a conexão com o banco de dados MySQL
+// Configuração da conexão com o banco de dados MySQL
 const bd = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'ifsuldeminas',
-  database: 'escola',
-  });
-  // Conecta ao banco de dados MySQL
-  bd.connect(err => {
-    if (err) {
-      console.error('Erro ao conectar ao MySQL:', err);
-      return;
-    }
-    console.log('Conectado ao banco de dados MySQL!');
-  });
-  
+  host: "localhost",
+  user: "root",
+  password: "ifsuldeminas",
+  database: "escola",
+});
+
+// Conecta ao banco de dados MySQL
+bd.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar ao MySQL:", err);
+    return;
+  }
+  console.log("Conectado ao banco de dados MySQL!");
+});
 
 // Rota principal
 app.get("/", (req, res) => {
@@ -50,13 +48,15 @@ app.get("/curso", (req, res) => {
     duracao: "18 meses",
     modalidade: "Presencial",
   };
-  res.send(` <h1> Página Curso<h1>
-             <p>Curso: ${curso.nome}</p>
-            <p>Duração: ${curso.duracao}</p>
-            <p>Modalidade: ${curso.modalidade}</p>`);
+  res.send(`
+    <h1>Página Curso</h1>
+    <p>Curso: ${curso.nome}</p>
+    <p>Duração: ${curso.duracao}</p>
+    <p>Modalidade: ${curso.modalidade}</p>
+  `);
 });
-// Rotas para tratar paginas não encontradas (404)
-// Esse bloco funciona como o else do servidor HTTP puro
+
+// Rota para páginas não encontradas (404)
 app.use((req, res) => {
   res.status(404).send("<h1>404 - Página Não Encontrada</h1>");
 });
